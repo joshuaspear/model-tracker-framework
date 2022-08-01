d9ace48cab47244212476253330ccc583455d66a
Joshua Spear <josh.spear9@gmail.com>
2022-08-01 15:11:30 +0100

    updated logging formatter

--- src/model_tracker_framework/__init__.py
+++ src/model_tracker_framework/__init__.py
@@ -4,8 +4,34 @@ from .SupervisedModelExperiment import SupervisedModelExperiment
 from .ModelTracker import ModelTracker
 from .MTFSupporting import ExperimentOption
 
-mtf_logger = logging.getLogger()
-console_handler = logging.StreamHandler()
+class CustomFormatter(logging.Formatter):
 
-mtf_logger.setLevel(logging.INFO)
-mtf_logger.addHandler(console_handler)
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+logger = logging.getLogger("mtf_logger")
+logger.setLevel(logging.INFO)
+
+# create console handler with a higher log level
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+console_handler.setFormatter(CustomFormatter())
+logger.addHandler(console_handler)
